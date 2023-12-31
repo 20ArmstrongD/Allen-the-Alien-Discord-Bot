a2b1270e13bc5a836b99e6dd6f4b54a65be6219e
20ArmstrongD <donald.curtis.armstrong@gmail.com>
2023-12-31 17:18:06 -0600

    made changes. they work but OPENAI being dumb

---
+++ index.mjs
@@ -0,0 +1,53 @@
+// create a discord bot using the openaai api that interacts on the discord server
+import 'dotenv/config'
+
+
+// prepare to connect to the discord api
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent 
+]})
+
+// prepare connection to openai api
+import OpenAI from 'openai';
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY,
+    maxRetries: 0,
+    organization: process.env.OPENAI_ORG
+})
+
+// check for when a message on discord is sent
+client.on('messageCreate', async function(message){
+    try {
+        // don't respond to yourself or other bots
+        if(message.author.bot) return;
+
+        const params = {
+            model: "gpt-3.5-turbo", 
+            prompt: `Allen is a nice guy.\n\
+Allen: Hello people of Earth, how do you do?\n\
+${message.author.username}: ${message.content}
+Allen:`,
+            //tempurature refers to how random the ai will be
+            // tempurature: 0.9,
+            max_tokens: 30,
+            // stop: ["Allen:", "BigDon(g)"],
+       }
+
+       
+        const AllenResponse = await openai.completions.create(params)
+       
+
+       console.log(completion.choices[0].text)
+        message.reply(`${AllenResponse.choices[0].text}`);
+        return;
+    } catch(err){
+        console.log(err)
+    }
+});
+
+// log the bot into discord
+client.login(process.env.DISCORD_TOKEN);
+console.log("Allen has made it to Earth")
